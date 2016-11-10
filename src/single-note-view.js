c3fc6e643d20e3d188b3b1473e69533fb9a08073
Amaal Ali <amq.ali@gmail.com>
2016-11-10 10:46:18 +0000

    Single note view returns string of HTML that represents the note model.

---
+++ src/single-note-view.js
@@ -0,0 +1,12 @@
+(function(exports) {
+
+  function SingleNoteView(note) {
+    this.note = note;
+  }
+
+  SingleNoteView.prototype.renderHTML = function () {
+    return "<div>" + this.note.returnText() + "</div>";
+  };
+
+  exports.SingleNoteView = SingleNoteView;
+})(this);
