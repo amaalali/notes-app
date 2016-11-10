b62d284f88c47f0025673d5a37e351474047053d
Amaal Ali <amq.ali@gmail.com>
2016-11-10 10:50:09 +0000

    Test for NoteListView to only display 20 char.

--- spec/unit/note-list-view-spec.js
+++ spec/unit/note-list-view-spec.js
@@ -9,9 +9,9 @@ function testNoteListViewReturningHtml() {
   var noteListView = new NoteListView(noteList);
   assert.isTrue( noteListView.printHtml() === "<ul></ul>");
   noteList.newNote('Favourite beer: Heineken');
-  assert.isTrue( noteListView.printHtml() === "<ul><li><div>Favourite beer: Heineken</div></li></ul>");
+  assert.isTrue( noteListView.printHtml() === "<ul><li><div>Favourite beer: Hein</div></li></ul>");
   noteList.newNote('Favourite editor: Vim');
-  assert.isTrue( noteListView.printHtml() === "<ul><li><div>Favourite beer: Heineken</div></li><li><div>Favourite editor: Vim</div></li></ul>");
+  assert.isTrue( noteListView.printHtml() === "<ul><li><div>Favourite beer: Hein</div></li><li><div>Favourite editor: Vi</div></li></ul>");
 }
 
 
